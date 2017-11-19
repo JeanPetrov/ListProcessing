07ac37bf4c693fed309c5b64fcad9fa7bc77c5a8
ginkogrudev <ginko404@gmail.com>
2017-11-19 23:20:14 +0200

    Added Insert Command
    Up for Review

---
+++ ListProcessing/ListProcessing/Commands/InsertCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace ListProcessing.Commands
+{
+    using Data.Contracts;
+    using Utilities;
+    using System.Collections.Generic;
+
+    public class InsertCommand : Command
+    {
+        public InsertCommand(IDataStorage dataStorage, IList<string> argsList) : base(dataStorage, argsList)
+        {
+
+        }
+
+        public override string Execute()
+        {
+            if (this.commandArgs.Count != 2)
+            {
+                return Constants.invalidParametersCountMessage;
+            }
+            this.dataStorage.List.Insert(int.Parse(this.commandArgs[0]), this.commandArgs[1]);
+
+            return string.Join(" ", this.dataStorage.List);
+        }
+    }
+}
