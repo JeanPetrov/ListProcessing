87a9406a7c4361728ae002ad5a2c50c09d381688
JeanPetrov <narumcyt@gmail.com>
2017-11-20 22:17:22 +0200

    Added CountCommand

---
+++ ListProcessing/ListProcessing/Commands/CountCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace ListProcessing.Commands
+{
+    using Data.Contracts;
+    using Utilities;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountCommand : Command
+    {
+        public CountCommand(IDataStorage dataStorage, IList<string> argsList) : base(dataStorage, argsList)
+        {
+
+        }
+
+        public override string Execute()
+        {
+            if (this.commandArgs.Count != 1)
+            {
+                return Constants.invalidParametersCountMessage;
+            }
+
+            return this.dataStorage.List.Where(w => w == this.commandArgs[0]).Count().ToString();
+        }
+    }
+}
